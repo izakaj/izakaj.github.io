bcacbcceec0d60aee95f09aa790d840a6283016d
izakaj <izakaj@gmail.com>
2019-02-17 09:13:44 +0100

    Another

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://stormy-retreat-46110.herokuapp.com/v1/tasks/';
+  const apiRoot = 'https://stormy-retreat-46110.herokuapp.com/v1/';
   const trelloApiRoot = 'https://stormy-retreat-46110.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
